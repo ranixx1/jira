d435120862e90d51bf2daeace719f41ac43f2133
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-02-10 02:19:50 -0300

    feat: adiciona criar e fechar chamado

--- src/main/java/com/example/jira/service/ChamadoService.java
+++ src/main/java/com/example/jira/service/ChamadoService.java
@@ -1,14 +1,37 @@
 package com.example.jira.service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.example.jira.enums.Escopo;
+import com.example.jira.enums.Role;
+import com.example.jira.enums.Status;
 import com.example.jira.model.Chamado;
-
+import com.example.jira.model.Usuario;
 
 public class ChamadoService {
     private List<Chamado> chamados = new ArrayList<>();
+
+    public void criarChamado(Role role, Usuario usuario, String titulo, String descricao, Escopo escopo) {
+        LocalDateTime agora = LocalDateTime.now();
+        Status statusInicial = Status.ABERTO;
+
+        Chamado novoChamado = new Chamado(role, usuario, agora, agora, statusInicial, titulo, descricao, escopo);
+        this.chamados.add(novoChamado);
+        System.out.println("Chamado aberto com sucesso!");
+    }
+
+    public void fecharChamado(Chamado chamado) {
+        if (chamado.getStatus() == Status.FECHADO) {
+            throw new IllegalArgumentException("O chamado já está fechado");
+        }
+        chamado.setStatus(Status.FECHADO);
+        chamado.setHorario_atualizacao(LocalDateTime.now()); // Atualiza e salva
+        System.out.println("Chamado #" + chamado.getId() + " fechado com sucesso!");
+    }
+
     public String listarChamados() {
         if (chamados.isEmpty()) {
             return "Lista de chamados está vazia";
