b7cb55ba3e4829013c56decf0830a82b7929862a
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-02-12 00:31:57 -0300

    feat: cria entidade comentário

---
+++ src/main/java/com/example/jira/model/Comentario.java
@@ -0,0 +1,63 @@
+package com.example.jira.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+
+@Table(name = "comentario")
+public class Comentario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String mensagem;
+
+    private LocalDateTime dataHoraCriacao;
+
+    @ManyToOne
+    private Usuario autor;
+
+    @ManyToOne
+    private Chamado chamado;
+
+    protected Comentario() {
+    }
+
+    public Comentario(String mensagem, Usuario autor, Chamado chamado) {
+        this.mensagem = mensagem;
+        this.autor = autor;
+        this.chamado = chamado;
+        this.dataHoraCriacao = LocalDateTime.now();
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public LocalDateTime getDataHoraCriacao() {
+        return dataHoraCriacao;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                comentário
+                ├─ Autor         : %s
+                    ├─ Mensagem  : %s
+                    ├─ Data/Hora : %s
+                """.formatted(
+                this.autor.getMatricula(),
+                this.getMensagem(),
+                this.getDataHoraCriacao()
+               );
+    }
+
+}
