fdeb54f5a7efa6e4476aef0293c1f6aa29bbe8a9
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-02-12 00:31:36 -0300

    feat: altera Enum e adiciona método comentar

--- src/main/java/com/example/jira/model/Chamado.java
+++ src/main/java/com/example/jira/model/Chamado.java
@@ -1,34 +1,43 @@
 package com.example.jira.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
 import com.example.jira.enums.Escopo;
 import com.example.jira.enums.Role;
 import com.example.jira.enums.Status;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.*;
-import java.time.LocalDateTime;
-
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "chamado")
-
 public class Chamado {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
     @NotNull(message = "O tipo é obrigatório")
     private Role role;
 
-    @Column(name = "criado_por")
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
     private Usuario usuario;
 
     @Column(name = "horario_abertura")
@@ -37,7 +46,7 @@ public class Chamado {
     @Column(name = "horario_atualização") // adicionar usúario que fez a ultima atualização
     private LocalDateTime horario_atualizacao;
 
-    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     @Column(name = "titulo")
@@ -48,8 +57,11 @@ public class Chamado {
     @NotNull(message = "A descrição é obrigatória")
     private String descricao;
 
-    @Column(name = "compartilhar_com")
+    @Enumerated(EnumType.STRING)
     private Escopo escopo;
+
+    @OneToMany(mappedBy = "chamado",cascade = CascadeType.ALL, orphanRemoval = false)
+    private List<Comentario>comentarios = new ArrayList<>();
 
     public Chamado(Role role, Usuario usuario, LocalDateTime horario_abertura, LocalDateTime horario_atualizazao,
             Status status, String titulo, String descricao, Escopo escopo) {
@@ -61,6 +73,11 @@ public class Chamado {
         this.status = status;
         this.descricao = descricao;
         this.escopo = escopo;
+    }
+
+    public void adicionarComentario(Comentario comentario){
+        this.comentarios.add(comentario);
+        this.horario_atualizacao = LocalDateTime.now();
     }
 
     @Override
