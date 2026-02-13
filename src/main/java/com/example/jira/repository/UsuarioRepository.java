27f93c73492fb0072de735b8c05d8aaadfe6ab86
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-02-13 01:21:28 -0300

    fix: adicionando marcação do @Repository

--- src/main/java/com/example/jira/repository/UsuarioRepository.java
+++ src/main/java/com/example/jira/repository/UsuarioRepository.java
@@ -1,12 +1,14 @@
 package com.example.jira.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.jira.enums.Time;
 import com.example.jira.model.Usuario;
 import java.util.Optional;
 import java.util.List;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Integer>{
     boolean existsByCpf(String cpf);
     Optional<Usuario>findByMatricula(String matricula);
